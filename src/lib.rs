3859b074168f1755ea0b43d7bb825d7d47080081
剑峰 王 <3303118@qq.com>
2020-03-13 12:16:11 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+#![forbid(unsafe_code)]
+extern crate url;
+extern crate html5ever;
+
+#[cfg(test)]
+#[macro_use]
+extern crate matches;
+
+pub mod classifier;
+pub mod speedreader;
